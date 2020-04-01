d30249fe40a5f7baa1283bc5590e61e21db2aa46
Natalie <ivanova_1999@inbox.ru>
2020-04-01 14:11:49 +0500

    Time tracker added | Minor fixes

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import numpy as np
 import math
+import pickle
+import time
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-
 from mpi4py import MPI
 
-import pickle
+
+class time_tracker():
+    start_time, end_time = time.time(), time.time()
+
+    def finish(self):
+        self.end_time = time.time()
+        ex_time = self.end_time - self.start_time
+        print("Execution time = {sec} seconds".format(sec=ex_time))
+
 
 ########
 # Явная центральная трехточечная схема
@@ -17,7 +26,7 @@ import pickle
 # u(t,0) = ψ(t), 0≤t≤T
 ########
 if __name__ == "__main__":
-    a = 0.25; T = 2; X = 1
+    a = 0.25; T = 4; X = 2
 
     φ = lambda x: (x*x*x)/(12*a*a)
     ψ = lambda t: (a*t*t*t)/12
@@ -35,6 +44,8 @@ if __name__ == "__main__":
     rank = comm.Get_rank()
     zone = math.floor(len(x)/(P))
 
+    execution = time_tracker()
+
     y = [0] * len(t)
     for i in range(len(t)):
         y[i] = [0] * len(x)
@@ -45,7 +56,27 @@ if __name__ == "__main__":
     for i in range(0, len(t)):
         y[i][0] = ψ(t[i]) # определяем граничные значения для t: u(t,0) = ψ(t)
 
+    # Если число процессов = 1
+    if (P == 1):
+        for i in range(0, len(t) - 1):
+            for j in range(1, len(x) - 1):
+                y[i+1][j] = 0.5*(y[i][j+1] + y[i][j-1]) - 0.5*(τ/h)*(y[i][j+1] - y[i][j-1]) + τ*f(t[i], x[j])
+            y[i+1][len(x) - 1] = (1-(τ/h))*y[i][len(x) - 1] + (τ/h)*(y[i][len(x) - 2]) + τ*(t[i])*(x[len(x) - 1])
+        
+        execution.finish()
 
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        x, t = np.meshgrid(x, t)
+        u = np.array(y)
+        ax.plot_surface(x, t, u)
+        ax.set_xlabel('x')
+        ax.set_ylabel('t')
+        ax.set_zlabel('u')
+        plt.show()
+
+
+    # Если число процессов != 1
     if (rank == 0): # в первой зоне
         start = 1
         end = zone - 1
@@ -54,7 +85,7 @@ if __name__ == "__main__":
             for j in range(start, end):
                 y[i+1][j] = 0.5*(y[i][j+1] + y[i][j-1]) - 0.5*(τ/h)*(y[i][j+1] - y[i][j-1]) + τ*f(t[i], x[j])
             y[i+1][end] = 0.5*(right + y[i][end-1]) - 0.5*(τ/h)*(right - y[i][end-1]) + τ*f(t[i], x[end])
-            req = comm.isend(y[i+1][end], dest=1, tag=0)
+            comm.isend(y[i+1][end], dest=1, tag=0)
             right = comm.recv(None, source=1, tag=0)
 
         comm.barrier() # синхронизация с остальными процессами
@@ -70,7 +101,7 @@ if __name__ == "__main__":
             for j in range(start, end):
                 y[i+1][j] = 0.5*(y[i][j+1] + y[i][j-1]) - 0.5*(τ/h)*(y[i][j+1] - y[i][j-1]) + τ*f(t[i], x[j])
             y[i+1][len(x) - 1] = (1-τ/h)*y[i][len(x) - 1] + (τ/h)*(y[i][len(x) - 2]) + τ*(t[i])*(x[len(x) - 1])
-            req = comm.isend(y[i+1][zone*rank], dest=P-2, tag=0)
+            comm.isend(y[i+1][zone*rank], dest=P-2, tag=0)
             left = comm.recv(None, source=P-2, tag=0)
 
         comm.barrier() # синхронизация с остальными процессами
@@ -91,8 +122,8 @@ if __name__ == "__main__":
                         y[i+1][j] = 0.5*(y[i][j+1] + y[i][j-1]) - 0.5*(τ/h)*(y[i][j+1] - y[i][j-1]) + τ*f(t[i], x[j])
                     y[i+1][end] = 0.5*(right + y[i][end-1]) - 0.5*(τ/h)*(right - y[i][end-1]) + τ*f(t[i], x[end])
                     
-                    reqLeft = comm.isend(y[i+1][zone*rank], dest=rank-1, tag=0)
-                    reqRight = comm.isend(y[i+1][end], dest=rank+1, tag=0)
+                    comm.isend(y[i+1][zone*rank], dest=rank-1, tag=0)
+                    comm.isend(y[i+1][end], dest=rank+1, tag=0)
                     left = comm.recv(None, source=rank-1, tag=0)
                     right = comm.recv(None, source=rank+1, tag=0) 
                     
@@ -117,6 +148,8 @@ if __name__ == "__main__":
                 for j in range(start, end):
                     y[i+1][j] = rec[i+1][j]
 
+        execution.finish()
+
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         x, t = np.meshgrid(x, t)
@@ -126,7 +159,3 @@ if __name__ == "__main__":
         ax.set_ylabel('t')
         ax.set_zlabel('u')
         plt.show()
-
-
-# /usr/local/bin/mpicc
-# mpiexec -n 4 python3 -m mpi4py mpi-feature.py
